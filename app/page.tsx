3d7bfba90fef78cbf642faa49c594ab50a444c68
haikaljs <haikaljavascript@gmail.com>
2023-09-22 16:07:01 +0800

    Global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p className="text-3xl font-medium text-sky-700">Hello world</p>;
+  return (
+    <p className="text-3xl font-medium text-sky-700">
+      <Button>Click me</Button>
+    </p>
+  );
 }
